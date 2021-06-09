ee72a98f9d091f7f8cf0da60472a1d1e1344534f
Anubhav Pandey <73362441+anubhav-pandey1@users.noreply.github.com>
2021-06-10 00:33:25 +0530

    Update upperbound.cpp with duplication compliance
    Reversing the low and high pointer traversal logic made the upperbound() duplication compliant.

--- Applications/upperbound.cpp
+++ Applications/upperbound.cpp
@@ -5,11 +5,11 @@ int binaryUpper(vector<int>& v, int n) {
     int lo = 0, hi = v.size() - 1;
     while (lo <= hi) {
         int mid = lo + (hi - lo) / 2;
-        if (v[mid] < n) {
-            lo = mid + 1;
+        if (v[mid] > n) {
+            hi = mid - 1;
         }
         else {
-            hi = mid - 1;
+            lo = mid + 1;
         }
     }
 
@@ -28,9 +28,9 @@ int binaryUpper(vector<int>& v, int n) {
 }
 
 int main() {
-    vector<int> v = {4, 9, 17, 21, 25, 29, 32, 35};
+    vector<int> v = {4, 4, 9, 17, 21, 25, 29, 35, 35};
 
-    int result1 = binaryUpper(v, 32);     // Test case 1
+    int result1 = binaryUpper(v, 4);      // Test case 1
     cout << result1 << endl;
 
     int result2 = binaryUpper(v, 35);     // Test case 2
