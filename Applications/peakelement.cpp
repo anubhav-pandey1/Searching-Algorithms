1b7325bb88cf1804820fd9ea306f790cc5746424
Anubhav Pandey <73362441+anubhav-pandey1@users.noreply.github.com>
2021-06-15 21:06:38 +0530

    Update peakelement.cpp with more solutions and testcases

--- Applications/peakelement.cpp
+++ Applications/peakelement.cpp
@@ -1,25 +1,25 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-int findPeakElement(vector<int>& v) { // Improve
-    if (v.size() < 2) {  // For small arrays (missed it)
+int findPeakElement(vector<int>& v) {   // Same logic, but uses 3 rules out of 4
+    if (v.size() < 2) {
         return 0;
     }
     int lo = 0, hi = v.size() - 1;
     while (lo <= hi) {
         int m = lo + (hi - lo) / 2;
-        if (m > 0 && m < v.size() - 1) { // Must be before else m - 1 not defined, missed
+        if (m > 0 && m < v.size() - 1) {
             if (v[m] > v[m + 1] && v[m] > v[m - 1]) {
                 return m;
             }
-            else if (v[m - 1] >= v[m + 1]) {  // Missed equality case
+            else if (v[m - 1] >= v[m]) {
                 hi = m - 1;
             }
-            else if (v[m - 1] < v[m + 1]) {
+            else if (v[m + 1] >= v[m]) {
                 lo = m + 1;
             }
         }
-        else if (m == 0) {        // Must be handled explicitly, missed
+        else if (m == 0) {
             if (v[0] > v[1]) {
                 return 0;
             }
@@ -36,7 +36,6 @@ int findPeakElement(vector<int>& v) { // Improve
             }
         }
     }
-
     return -1;
 }
 
@@ -67,6 +66,55 @@ int main() {
 
     return 0;
 }
+
+// Below are the four rules that determine which side contains peak element:-
+// a. If num[i-1] < num[i] > num[i+1], then num[i] is peak
+// b. If num[i-1] < num[i] < num[i+1], then num[i+1...n-1] must contains a peak
+// c. If num[i-1] > num[i] > num[i+1], then num[0...i-1] must contains a peak
+// d. If num[i-1] > num[i] < num[i+1], then both sides have peak
+// The rule b and d can merge to one rule: if nums[i] < nums[i+1], then nums[i+1…n-1] must contains a peak element
+// The rule c and d can merge to one rule: if nums[i] > nums[i+1], then nums[0…i-1] must contains a peak element
+
+// Improved, somewhat concise code:-
+
+// int findPeakElement(vector<int>& v) { // Improved, somewhat concise code but uses all 4 rules
+//     if (v.size() < 2) {  // For small arrays (missed it)
+//         return 0;
+//     }
+//     int lo = 0, hi = v.size() - 1;
+//     while (lo <= hi) {
+//         int m = lo + (hi - lo) / 2;
+//         if (m > 0 && m < v.size() - 1) { // Must be before else m - 1 not defined, missed
+//             if (v[m] > v[m + 1] && v[m] > v[m - 1]) {
+//                 return m;
+//             }
+//             else if (v[m - 1] >= v[m + 1]) {  // Missed equality case
+//                 hi = m - 1;
+//             }
+//             else if (v[m - 1] < v[m + 1]) {
+//                 lo = m + 1;
+//             }
+//         }
+//         else if (m == 0) {        // Must be handled explicitly, missed
+//             if (v[0] > v[1]) {
+//                 return 0;
+//             }
+//             else {
+//                 return 1;
+//             }
+//         }
+//         else if (m == v.size() - 1) {
+//             if (v[m] > v[m - 1]) {
+//                 return m;
+//             }
+//             else {
+//                 return m - 1;
+//             }
+//         }
+//     }
+
+//     return -1;
+// }
 
 // My initial, inconcise code:-
 
@@ -129,6 +177,8 @@ int main() {
 // 1 2 3 4 3
 // 5
 // 1 2 1 2 1
+// 2
+// 2 1
 
 // Outputs (can have other outputs too):
 
